7c7e11efe32d503cd6825528462ea13bcb6125d2
dubbelosix <th3revanchist@gmail.com>
2024-05-09 06:21:33 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ set -x
 install_packages() {
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "Linux..."
+        # remove annoying popup for daemon restart https://stackoverflow.com/questions/73397110/how-to-stop-ubuntu-pop-up-daemons-using-outdated-libraries-when-using-apt-to-i#comment131834051_73397970
+        sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
         curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
         sudo apt-get install -y vim clangd nodejs universal-ctags build-essential
     elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -17,9 +19,6 @@ install_packages() {
         exit 1
     fi
 }
-
-# remove annoying popup for daemon restart https://stackoverflow.com/questions/73397110/how-to-stop-ubuntu-pop-up-daemons-using-outdated-libraries-when-using-apt-to-i#comment131834051_73397970
-sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 
 # install vim + deps
 install_packages
